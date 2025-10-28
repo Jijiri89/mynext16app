5a43f67e2b8c45a0b5c2e47e13b787c7a8212e22
Jijiri <jijirijohnson.com>
2025-10-28 22:27:59 +0000

    added counter app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+'use client'
 import Image from "next/image";
+import { useState } from "react";
 
-export default function Home() {
+function Home() {
+  const [counter,setCounter]=useState(0);
+  const increaseCount=()=>{
+    setCounter(c=>c+1);
+  }
+
+  const decreaseCount=()=>{
+   if(counter>0)
+     setCounter(c=>c-1);
+  }
+
   return (
-    <div className="m-5">Hello Nextjs 16
-    
+    <div className="m-5 ">Hello Nextjs 16
+    <p className="px-10">{counter}</p>
+    <br />
+    <button className="px-10 my-2 bg-blue-700 text-white" onClick={increaseCount}>Add</button>
+    <br />
+    <button className="px-5 bg-red-700 text-white" onClick={decreaseCount}>Decrease</button>
     </div>
    
   );
 }
+export default Home;
